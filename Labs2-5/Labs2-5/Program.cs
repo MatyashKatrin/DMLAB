79eb7235b0ff979cd225479c6d9d1c52b4470dcd
Your Name <you@example.com>
2023-04-11 12:41:28 +0200

    first commit

--- Labs2-5/Labs2-5/Program.cs
+++ Labs2-5/Labs2-5/Program.cs
@@ -1,7 +1,13 @@
-﻿using Labs_DM.Lab2;
+﻿using System.Text;
+
+using Labs_DM.Lab2;
 using Labs_DM.Lab3;
 using Labs_DM.Lab4;
+using Labs_DM.Lab5;
 
-Lab2_Postman.Lab2_Execute();
+Console.OutputEncoding = Encoding.UTF8;
+
+//Lab2_Postman.Lab2_Execute();
 //Lab3_Salesman.Lab3_Execute();
 //Lab4_Chanels.Lab4_Execute();
+Lab5_Isomorphism.Lab5_Execute();
